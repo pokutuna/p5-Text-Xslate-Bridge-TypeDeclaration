76a747395c818db33fa081b300f5ed7094cdde1e
pokutuna <popopopopokutuna@gmail.com>
2017-01-27 10:45:09 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Text::Xslate::Bridge::TypeDeclaration - It's new $module
+
+# SYNOPSIS
+
+    use Text::Xslate::Bridge::TypeDeclaration;
+
+# DESCRIPTION
+
+Text::Xslate::Bridge::TypeDeclaration is ...
+
+# LICENSE
+
+Copyright (C) pokutuna.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+pokutuna <popopopopokutuna@gmail.com>
---
+++ lib/Text/Xslate/Bridge/TypeDeclaration.pm
@@ -0,0 +1,39 @@
+package Text::Xslate::Bridge::TypeDeclaration;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Text::Xslate::Bridge::TypeDeclaration - It's new $module
+
+=head1 SYNOPSIS
+
+    use Text::Xslate::Bridge::TypeDeclaration;
+
+=head1 DESCRIPTION
+
+Text::Xslate::Bridge::TypeDeclaration is ...
+
+=head1 LICENSE
+
+Copyright (C) pokutuna.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+pokutuna E<lt>popopopopokutuna@gmail.comE<gt>
+
+=cut
+
