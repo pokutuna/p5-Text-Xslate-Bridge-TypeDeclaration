4d3d3a0a47c7bd41f8bf710381207771a8d0ce74
pokutuna <popopopopokutuna@gmail.com>
2017-07-17 15:15:18 +0900

    Checking in changes prior to tagging of version 0.10.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 390401a..2d1f377 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,7 +2,12 @@ Revision history for Perl extension Text-Xslate-Bridge-TypeDeclaration
    
     {{$NEXT}}
    
    +0.10 2017-07-17T06:15:04Z
    +    [BREAKING RELEASE]
    +    - Using Type::Tiny in #3.
    +    - ArrayRef no longer treat as a Tuple type.
    +    - Detecting the ouput format to escape. The `print` option will not take format.
    +
     0.01 2017-06-26T09:15:56Z
    
         - original version
    -

--- lib/Text/Xslate/Bridge/TypeDeclaration.pm
+++ lib/Text/Xslate/Bridge/TypeDeclaration.pm
@@ -12,7 +12,7 @@ use Type::Registry ();
 use Type::Tiny qw();
 use Types::Standard qw(Any Dict slurpy);
 
-our $VERSION = '0.01';
+our $VERSION = '0.10';
 
 # Set truthy value to skip validation for local scope.
 our $DISABLE_VALIDATION = 0;
